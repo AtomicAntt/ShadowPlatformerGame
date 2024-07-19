341d0273f80afae32a29d9394a27c3e38892ab29
unknown <anthonychi36@gmail.com>
2024-07-18 23:14:50 -0700

    Refactor player script to move using states for organization purposes.

--- GameObjects/Player.gd
+++ GameObjects/Player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+enum States {AIR, FLOOR, DEAD}
+var state: States = States.AIR
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -7,22 +9,32 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process(delta: float):
 	move_and_slide()
+	fall(delta)
+	
+	match state:
+		States.AIR:
+			get_horizontal_movement()
+			if is_on_floor():
+				state = States.FLOOR
+		States.FLOOR:
+			get_horizontal_movement()
+			if Input.is_action_pressed("up"):
+				velocity.y += JUMP_VELOCITY
+				state = States.AIR
+		States.DEAD:
+			pass
+	
+func get_horizontal_movement() -> void:
+	if Input.is_action_pressed("right"):
+		velocity.x = lerp(velocity.x, SPEED, 0.2)
+		$AnimatedSprite2D.flip_h = false
+	elif Input.is_action_pressed("left"):
+		velocity.x = lerp(velocity.x, -SPEED, 0.2)
+		$AnimatedSprite2D.flip_h = true
+	else:
+		velocity.x = lerp(velocity.x, 0.0, 0.2)
+
+func fall(delta: float) -> void:
+	velocity.y += gravity * delta
