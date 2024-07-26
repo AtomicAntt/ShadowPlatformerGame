48c950b100866742e7d93ea35220e8f8c11d261a
unknown <anthonychi36@gmail.com>
2024-07-25 17:46:36 -0700

    Disable raycasts when lights are turned off and re-enable them after they are turned back on

--- GameObjects/Light.gd
+++ GameObjects/Light.gd
@@ -5,7 +5,8 @@ var interactable = false
 func _input(event):
 	if event.is_action_pressed("interact") and interactable:
 		$PointLight2D.enabled = !$PointLight2D.enabled
-
+		for raycast in $PointLight2D.raycasts:
+			raycast.enabled = $PointLight2D.enabled
 
 func _on_light_switch_area_entered(area):
 	if area.is_in_group("PlayerHitbox"):
