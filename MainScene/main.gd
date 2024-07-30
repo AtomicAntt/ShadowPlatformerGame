8446d1f832d1b3547feba2565bc74aef307b99d1
unknown <anthonychi36@gmail.com>
2024-07-29 22:14:02 -0700

    Add HTML export and fix HTML export related bugs

--- MainScene/main.gd
+++ MainScene/main.gd
@@ -4,10 +4,14 @@ extends Control
 
 var current_level: int = 0
 
+var level_0 = preload("res://Levels/Level0.tscn") # Ugh, I have to do this because html export doesn't work otherwise..
+
 var level_instance: Node2D
 
 #func _ready():
-	#load_level("Level0")
+	#$MainMenu.visible = false	
+	#var level_instance = level_0.instantiate()
+	#main_2d.add_child(level_instance)
 
 func game_over() -> void:
 	$Main2D/CanvasLayer/GameOver.visible = true
@@ -22,10 +26,15 @@ func load_level(level_name : String) -> void:
 	unload_level()
 	var level_path: String = "res://Levels/%s.tscn" % level_name
 	var level_resource: Resource = load(level_path)
+	#var level_resource: Resource
+	#match level_name:
+		#"level0":
+			#level_resource = level_0
+	
 	if level_resource:
 		level_instance = level_resource.instantiate()
 		main_2d.add_child(level_instance)
-		
+	
 	match current_level:
 		0: 
 			$Main2D/CanvasLayer/Dialogue.start_level_0_text()
@@ -39,7 +48,7 @@ func reload_level() -> void:
 	load_level("Level"+str(current_level))
 
 func _on_start_pressed():
-	load_level("level0")
+	load_level("Level0")
 
 func _on_restart_pressed():
 	reload_level()
